2964bd1661e38a11d1e0988ea8b3e173668eab0f
Sami BEN ELHAJ SALAH <sami.bhs.91@gmail.com>
2019-12-25 18:12:10 +0100

    copie

--- hello.cpp
+++ hello.cpp
@@ -4,6 +4,7 @@ int main()
 {
      cout<<"Hello world"<<endl;
      cout<<"I am programmer"<<endl;
-	 cout<<"modifications 2";
+	 cout<<"modifications 2"<<endl;
+	 cout<<"copie"<<endl;
      return 0;
 }
